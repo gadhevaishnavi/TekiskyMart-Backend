f7c70ff7e2d75cc499133adbf1a1181294b58316
vaishnavi gadhe <gadhevaishnavi70@gmail.com>
2025-02-05 16:28:14 +0530

    changes

--- routers/preOrderRoutes.js
+++ routers/preOrderRoutes.js
@@ -6,15 +6,15 @@ import {
   handleUpdatePreOrderById,
   handleDeletePreOrderById,
 } from '../controllers/preOrderController.js';
-import { authenticateToken } from '../auth/jwtToken.js'; // Authentication middleware
+// import { authenticateToken } from '../auth/jwtToken.js'; // Authentication middleware
 
 const preOrderRoute = express.Router();
 
 // Protected routes with authenticateToken middleware
-preOrderRoute.post('/', authenticateToken, handleCreatePreOrder); // Create a pre-order
-preOrderRoute.get('/', authenticateToken, handleGetAllPreOrders); // Get all pre-orders
-preOrderRoute.get('/:id', authenticateToken, handleGetPreOrderById); // Get pre-order by ID
-preOrderRoute.put('/:id', authenticateToken, handleUpdatePreOrderById); // Update pre-order by ID
-preOrderRoute.delete('/:id', authenticateToken, handleDeletePreOrderById); // Delete pre-order by ID
+preOrderRoute.post('/',  handleCreatePreOrder); // Create a pre-order
+preOrderRoute.get('/', handleGetAllPreOrders); // Get all pre-orders
+preOrderRoute.get('/:id', handleGetPreOrderById); // Get pre-order by ID
+preOrderRoute.put('/:id',  handleUpdatePreOrderById); // Update pre-order by ID
+preOrderRoute.delete('/:id',  handleDeletePreOrderById); // Delete pre-order by ID
 
 export default preOrderRoute;
