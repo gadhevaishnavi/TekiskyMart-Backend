edfff05e720bf60949814dd61ed26d7f667fbd89
Nehasontakke123 <nehasontakke1880@gmail.com>
2025-02-05 18:21:07 +0530

    create get one product api

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,6 +16,24 @@ export const getProduct = async (req, res) => {
   }
 };
 
+//get one product
+
+export const getProductById = async (req, res) => {
+    try {
+        const productId = req.params.productId
+
+        const product = await productModel.findById(productId);
+        
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
 
 
 export const createProduct = async (req, res) => {
